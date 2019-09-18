1aa9f10362f5e2d542b28b7ac11a26fe865ddb35
Élie <e.sarfati@rubika-edu.com>
2019-09-18 23:51:09 +0200

    import project

---
+++ VideoMappingDrawingTool/Assets/1_Scripts/MouseCross.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DrawingTool
+{
+    public class MouseCross : MonoBehaviour
+    {
+        
+        [SerializeField] private Mode mode = default;
+        [SerializeField] private int size = 3;
+
+        [SerializeField] private Camera cam = default;
+        [SerializeField] private RectTransform rectTransform = default;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Resize();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            FollowMouse();
+        }
+
+        private void Resize()
+        {
+            if(mode == Mode.Horizontal) rectTransform.sizeDelta = new Vector2(Screen.width, size);
+            else rectTransform.sizeDelta = new Vector2(size, Screen.height);
+        }
+
+        private void FollowMouse()
+        {
+            if (mode == Mode.Horizontal) rectTransform.anchoredPosition = new Vector2(0, Input.mousePosition.y);
+            else rectTransform.anchoredPosition = new Vector2(Input.mousePosition.x, 0);
+        }
+
+        public enum Mode { Vertical, Horizontal};
+    }
+}
---
+++ VideoMappingDrawingTool/Assets/1_Scripts/TextureHandler.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DrawingTool
+{
+    public class TextureHandler : MonoBehaviour
+    {
+        [SerializeField] private RawImage image = default;
+        [SerializeField] private Color color = Color.white;
+        [SerializeField] private int brushSize = 1;
+
+        private Texture2D texture;
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            SetTexture();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButton(0)) Draw(color);
+        }
+
+        private void Draw(Color _color)
+        {
+            int x = (int)Input.mousePosition.x;
+            int y = (int)Input.mousePosition.y;
+
+            for (int i = x - brushSize; i < x+brushSize; i++)
+            {
+                for (int j = y-brushSize; j < y+brushSize; j++)
+                {
+                    texture.SetPixel(i, j, _color);
+                }
+            }
+
+            
+            texture.Apply();
+            image.texture = texture;
+        }
+
+        private void BrushSize()
+        {
+
+        }
+
+        private void SetTexture()
+        {
+            image.texture = new Texture2D(Screen.width, Screen.height);
+            texture = image.texture as Texture2D;
+            for (int x = 0; x < Screen.width; x++)
+            {
+                for (int y = 0; y < Screen.height; y++)
+                {
+                    texture.SetPixel(x, y, Color.black);
+
+                }
+            }
+            texture.Apply();
+            image.texture = texture;
+        }
+    }
+}
